abb8c1a7e887a1082ace247faa1ce2b8606b5af3
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-10-28 12:19:22 +0900

    Add files via upload

--- prac2.ino
+++ prac2.ino
@@ -1,39 +1,41 @@
-#define UPPER_RED 2
-#define UPPER_YELLOW 3
-#define UPPER_GREEN 4 //위쪽 불
-#define LOWER_RED 22
-#define LOWER_YELLOW 24
-#define LOWER_GREEN 26  //아래쪽 불
-#define RIGHT_RED 11
-#define RIGHT_YELLOW 12
-#define RIGHT_GREEN 10  //오른쪽 불
-#define LEFT_RED 23
-#define LEFT_YELLOW 25
-#define LEFT_GREEN 27  //왼쪽 불
+#define Up_Red 2
+#define Up_Yellow 3
+#define Up_Green 4 //위쪽 불
+#define Down_Red 22
+#define Down_Yellow 24
+#define Down_Green 26  //아래쪽 불
+#define Right_Red 11
+#define Right_Yellow 12
+#define Right_Green 10  //오른쪽 불
+#define Left_Red 23
+#define Left_Yellow 25
+#define Left_Green 27  //왼쪽 불
 #define SW 30       //위아래 스위치
 
+unsigned long TimeUD;
+unsigned long TimeLR;
 
-int timePinRow[8] = { 40, 41, 42, 43, 44, 45, 46, 47 };
+int timePinRow[8] = { 40, 41, 42, 43, 44, 45, 46, 47 };//정지신호를 출력하기위한 핀번호 배열
 int timePinCol[8] = { 32, 33, 34, 35, 36, 37, 38, 39 };
-// 보행자 신호 출력용 매트릭스 핀 번호 배열
-int signPinRow[8] = { A8, A9, A10, A11, A12, A13, A14, A15 };
+
+int signPinRow[8] = { A8, A9, A10, A11, A12, A13, A14, A15 };//보행신호와 시간(숫자)를 출력하는 핀번호 배열
 int signPinCol[8] = { A0, A1, A2, A3, A4, A5, A6, A7 };
 
 //기타 기호상수
-#define DRIVABLE_DURATION           10000u                                                  //주행 가능 시간, 녹색등 유지시간(20sec)
-#define LED_YELLOW_BLINK_DURATION   5000u                                                   //경고등 점멸 시간(5sec)
-#define BLINK_TIME                  LED_YELLOW_BLINK_DURATION / 1000                        //점멸 횟수(1초당 1회)
-#define TRAFFIC_LIGHT_IDLE_TIME     3000u                                                   //입력 받았을 시 대기 시간(3sec)
-#define ARRAY_LENGTH                8                                                       //배열 길이
-#define CYCLE                       (DRIVABLE_DURATION + LED_YELLOW_BLINK_DURATION) / 1000  //신호 바뀔 때까지의 걸리는 시간
-#define WALK_DURATION               CYCLE * 1000                                            //
+#define DRIVABLE_DURATION           10000u   //주행 가능 시간
+#define LED_Yellow_BLINK_DURATION   5000u    //경고등 점멸 시간(5sec)
+#define BLINK_TIME                  LED_Yellow_BLINK_DURATION / 1000 //점멸 횟수 (1초당1회),for문에서 조건으로 사용하기위해 1000으로 나눠줌
+#define LED_DELAY_TIME              3000u    //입력 받았을 시 3초의 대기시간을 갖기위해
+#define ARRAY_LENGTH                8        //열 길이
+#define CYCLE                       (DRIVABLE_DURATION + LED_Yellow_BLINK_DURATION) / 1000  //현재 15000/1000 = 15 로 다음 신호로 바뀌는데까지 걸리는시간(초록불시간+점멸시간)
+#define WALK_DURATION               CYCLE * 1000    //보행자신호를 출력하기위한(DisplayWalkSign) 시간을 설정한 변수 현재 15000
 
-//폴링시 필요한 변수
-bool gbChange = false;
+
+bool state = false; //  false(위아래 빨간불 양옆 파란불), true(위아래 파란불 양옆 빨간불)
 //DotMatrix 타이머 출력을 위한 변수
-int gNumbers[10][8][4] =
+int Numbers[10][8][4] =
 {
-  // ZERO
+  // 숫자 0
   {
     { 0, 1, 1, 0 },
     { 1, 0, 0, 1 },
@@ -44,7 +46,7 @@ int gNumbers[10][8][4] =
     { 1, 0, 0, 1 },
     { 0, 1, 1, 0 }
   },
-  // ONE
+  // 숫자 1
   {
     { 0, 0, 1, 0 },
     { 0, 1, 1, 0 },
@@ -55,7 +57,7 @@ int gNumbers[10][8][4] =
     { 0, 0, 1, 0 },
     { 1, 1, 1, 1 }
   },
-  // TWO
+  // 숫자 2
   {
     { 0, 1, 1, 0 },
     { 1, 0, 0, 1 },
@@ -66,7 +68,7 @@ int gNumbers[10][8][4] =
     { 1, 0, 0, 0 },
     { 1, 1, 1, 1 }
   },
-  // THREE
+  // 숫자 3
   {
     { 0, 1, 1, 0 },
     { 1, 0, 0, 1 },
@@ -77,7 +79,7 @@ int gNumbers[10][8][4] =
     { 1, 0, 0, 1 },
     { 0, 1, 1, 0 }
   },
-  // FOUR
+  // 숫자 4
   {
     { 0, 0, 1, 0 },
     { 0, 1, 1, 0 },
@@ -88,7 +90,7 @@ int gNumbers[10][8][4] =
     { 0, 0, 1, 0 },
     { 0, 0, 1, 0 }
   },
-  // FIVE
+  // 숫자 5
   {
     { 1, 1, 1, 1 },
     { 1, 0, 0, 0 },
@@ -99,7 +101,7 @@ int gNumbers[10][8][4] =
     { 0, 0, 0, 1 },
     { 1, 1, 1, 1 }
   },
-  // SIX
+  // 숫자 6
   {
     { 0, 1, 1, 0 },
     { 1, 0, 0, 1 },
@@ -110,7 +112,7 @@ int gNumbers[10][8][4] =
     { 1, 0, 0, 1 },
     { 0, 1, 1, 0 }
   },
-  // SEVEN
+  // 숫자 7
   {
     { 1, 1, 1, 1 },
     { 0, 0, 0, 1 },
@@ -121,7 +123,7 @@ int gNumbers[10][8][4] =
     { 0, 1, 0, 0 },
     { 0, 1, 0, 0 }
   },
-  // EIGHT
+  // 숫자 8
   {
     { 0, 1, 1, 0 },
     { 1, 0, 0, 1 },
@@ -132,7 +134,7 @@ int gNumbers[10][8][4] =
     { 1, 0, 0, 1 },
     { 0, 1, 1, 0 }
   },
-  // NINE
+  // 숫자 9
   {
     { 0, 1, 1, 0 },
     { 1, 0, 0, 1 },
@@ -145,86 +147,49 @@ int gNumbers[10][8][4] =
   }
 };
 //DotMatrix 보행자 그림 출력을 위한 변수
-int gWalkerSigns[2][8][8] =
+int WalkerSigns[2][8][8] =
 {
-  // RED : CANNOT CROSS
+  // 보행자 빨간불
   {
-    { 0, 0, 0, 1, 1, 0, 0, 0 },
-    { 0, 0, 0, 1, 1, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 1, 1, 1, 1, 0, 0 },
-    { 0, 1, 0, 1, 1, 0, 1, 0 },
-    { 0, 0, 0, 1, 1, 0, 0, 0 },
-    { 0, 0, 1, 0, 0, 1, 0, 0 },
-    { 0, 0, 1, 0, 0, 1, 0, 0 }
+   { 0, 0, 0, 1, 1, 0, 0, 0 },
+   { 0, 0, 0, 1, 1, 0, 0, 0 },
+   { 0, 0, 1, 1, 1, 1, 0, 0 },
+   { 0, 0, 1, 1, 1, 1, 0, 0 },
+   { 0, 0, 1, 1, 1, 1, 0, 0 },
+   { 0, 0, 1, 1, 1, 1, 0, 0 },
+   { 0, 0, 0, 1, 1, 0, 0, 0 },
+   { 0, 0, 0, 1, 1, 0, 0, 0 }
   },
-  // GREEN : CAN CROSS
-  {
-    { 0, 0, 0, 1, 1, 0, 0, 0 },
-    { 0, 0, 0, 1, 1, 0, 0, 0 },
-    { 0, 1, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 1, 1, 1, 1, 1, 0 },
-    { 0, 0, 0, 1, 1, 0, 0, 0 },
-    { 0, 0, 0, 1, 1, 0, 1, 0 },
-    { 0, 0, 1, 0, 0, 1, 0, 0 },
-    { 0, 1, 0, 0, 0, 0, 0, 0 }
-  }
+
+  // 보행자 초록불
+{
+{0,0,0,1,1,0,0,0},
+{0,0,0,1,1,0,0,0},
+{0,0,1,1,1,1,0,0},
+{0,0,1,1,1,1,0,0},
+{0,0,1,1,1,1,0,0},
+{1,1,0,1,1,0,1,0},
+{0,0,0,1,1,0,0,1},
+{0,0,1,0,1,0,0,0},
+}
 };
-/*
-//좌우를 위한 함수
-//기능 : 좌우의 노란등을 한번 점멸합니다.
-void BlinkLEDForLR();
-//기능 : duration 동안 보행 신호를 출력하면서 폴링을 받습니다.
-//인자 : duration은 ms단위입니다.
-void PrintWalkSignWithPolling(int duration);
-//기능 : 신호를 바꿉니다.
-void AdjustTrafficLightForLR();
 
-//위아래를 위한 함수
-//기능 : 위아래의 노란등을 한번 점멸합니다.
-//인자 : former는 출력할 수의 십의 자리, later는 출력할 수의 일의 자리입니다.
-void BlinkLEDForUD(int former, int later);
-//기능 : 신호를 바꿉니다.
-void AdjustTrafficLightForUD();
-//기능 : duration 동안 타이머를 출력하면서 폴링을 받습니다.
-//인자 : duration은 ms단위이고, former는 출력할 수의 십의 자리, later는 출력할 수의 일의 자리입니다.
-void PrintStopSignWithPolling(int duration, int former, int later);
-
-//공통 함수
-//기능 : 상단의 DotMatrix 신호를 지웁니다.
-void ClearUpper();
-//기능 : 하단의 DotMatrix 신호를 지웁니다.
-void ClearLower();
-//기능 : former, later순으로 DotMatrix에 숫자를 출력합니다.
-//인자 : former는 출력할 수의 십의 자리, later는 출력할 수의 일의 자리입니다.
-void DisplayNumber(int former[][4], int later[][4]);
-//기능 : 상단 DotMatrix에 정지 신호를 출력합니다.
-void DisplayStopSign();
-//기능 : 하단 DotMatrix에 보행 신호를 출력합니다.
-void DisplayWalkSign();
-//기능 : duration 동안 타이머를 출력하면서 대기합니다.
-//인자 : duration은 ms 단위, former는 출력할 수의 십의 자리, later는 출력할 수의 일의 자리입니다.
-void PrintStopSign(int duration, int former, int later);
-//기능 : duration동안 보행 신호를 출력합니다.
-//인자 : duration은 ms단위입니다.
-void PrintWalkSign(unsigned duration);
-*/
 void setup()
 {
   //상하 출력 핀 모드 설정
-  pinMode(UPPER_RED, OUTPUT);
-  pinMode(LOWER_RED, OUTPUT);
-  pinMode(UPPER_YELLOW, OUTPUT);
-  pinMode(LOWER_YELLOW, OUTPUT);
-  pinMode(UPPER_GREEN, OUTPUT);
-  pinMode(LOWER_GREEN, OUTPUT);
+  pinMode(Up_Red, OUTPUT);
+  pinMode(Down_Red, OUTPUT);
+  pinMode(Up_Yellow, OUTPUT);
+  pinMode(Down_Yellow, OUTPUT);
+  pinMode(Up_Green, OUTPUT);
+  pinMode(Down_Green, OUTPUT);
   //좌우 출력 핀 모드 설정
-  pinMode(LEFT_RED, OUTPUT);
-  pinMode(RIGHT_RED, OUTPUT);
-  pinMode(LEFT_YELLOW, OUTPUT);
-  pinMode(RIGHT_YELLOW, OUTPUT);
-  pinMode(LEFT_GREEN, OUTPUT);
-  pinMode(RIGHT_GREEN, OUTPUT);
+  pinMode(Left_Red, OUTPUT);
+  pinMode(Right_Red, OUTPUT);
+  pinMode(Left_Yellow, OUTPUT);
+  pinMode(Right_Yellow, OUTPUT);
+  pinMode(Left_Green, OUTPUT);
+  pinMode(Right_Green, OUTPUT);
   //DotMatrix 핀 모드 설정 및 초기화
   for(int i = 0; i < ARRAY_LENGTH; ++i)
   {
@@ -240,171 +205,202 @@ void setup()
   }
   //입력 핀 모드 설정
   pinMode(SW, INPUT_PULLUP);
+    checkLED();
+    checkDotmatrix();
 }
 
 void loop()
 {
-  //상하 주행
-  digitalWrite(LEFT_RED, HIGH);
-  digitalWrite(RIGHT_RED, HIGH);
-  digitalWrite(UPPER_GREEN, HIGH);
-  digitalWrite(LOWER_GREEN, HIGH);
+  //상하 주행state = true
+  digitalWrite(Left_Red, HIGH);
+  digitalWrite(Right_Red, HIGH);
+  digitalWrite(Up_Green, HIGH);
+  digitalWrite(Down_Green, HIGH);
   for (int t = CYCLE; t > 0; --t)
   {
      int former = t / 10;
      int later = t % 10;
      if (t > BLINK_TIME)
      {
-        PrintStopSignWithPolling(1000, former, later);
-        if (gbChange)
+        DisplayStopSignWithSwitch(1000, former, later);
+        if (state == true)
        {
-        gbChange = false;
+        state = false;
         break;
        }
      }
-     else if (digitalRead(UPPER_RED) == LOW && digitalRead(LOWER_RED) == LOW)
+     else if (digitalRead(Up_Red) == LOW && digitalRead(Down_Red) == LOW)
      {
-       digitalWrite(UPPER_GREEN, LOW);
-       digitalWrite(LOWER_GREEN, LOW);
-       BlinkLEDForUD(former, later);
+       digitalWrite(Up_Green, LOW);
+       digitalWrite(Down_Green, LOW);
+       UD_BlinkYellow(former, later);
      }
   }
-  digitalWrite(LEFT_RED, LOW);
-  digitalWrite(RIGHT_RED, LOW);
+  digitalWrite(Left_Red, LOW);
+  digitalWrite(Right_Red, LOW);
 
   //좌우 주행
-  digitalWrite(UPPER_RED, HIGH);
-  digitalWrite(LOWER_RED, HIGH);
-  digitalWrite(LEFT_GREEN, HIGH);
-  digitalWrite(RIGHT_GREEN, HIGH);
+  digitalWrite(Up_Red, HIGH);
+  digitalWrite(Down_Red, HIGH);
+  digitalWrite(Left_Green, HIGH);
+  digitalWrite(Right_Green, HIGH);
   for (int t = CYCLE; t > 0; --t)
   {
      int former = t / 10;
      int later = t % 10;
      if (t > BLINK_TIME)
      {
-        PrintWalkSignWithPolling(1000);
-        if(gbChange)
+        DisplayWalkSign(1000);
+        if(state == true)
        {
-        gbChange = false;
+        state = false;
         break;
        }
      }
-     else if (digitalRead(LEFT_RED) == LOW && digitalRead(RIGHT_RED) == LOW)
+     else if (digitalRead(Left_Red) == LOW && digitalRead(Right_Red) == LOW)
      {
-       digitalWrite(LEFT_GREEN, LOW);
-       digitalWrite(RIGHT_GREEN, LOW);
-       BlinkLEDForLR();
+       digitalWrite(Left_Green, LOW);
+       digitalWrite(Right_Green, LOW);
+       LR_BlinkYellow();
      }
   }
-  digitalWrite(UPPER_RED, LOW);
-  digitalWrite(LOWER_RED, LOW);
+  digitalWrite(Up_Red, LOW);
+  digitalWrite(Down_Red, LOW);
 }
 
-void BlinkLEDForLR()
-{
-  static const int delayTime = LED_YELLOW_BLINK_DURATION / (BLINK_TIME + BLINK_TIME);
+
+ void checkLED() {
+  digitalWrite(Right_Green, HIGH);delay(100);digitalWrite(Right_Green, LOW);
+  digitalWrite(Right_Yellow, HIGH);delay(100);digitalWrite(Right_Yellow, LOW);
+  digitalWrite(Right_Red, HIGH);delay(100);digitalWrite(Right_Red, LOW);
+  digitalWrite(Up_Green, HIGH);delay(100);digitalWrite(Up_Green, LOW);
+  digitalWrite(Up_Yellow, HIGH);delay(100);digitalWrite(Up_Yellow, LOW);
+  digitalWrite(Up_Red, HIGH);delay(100);digitalWrite(Up_Red, LOW);
+  digitalWrite(Left_Red, HIGH);delay(100);digitalWrite(Left_Red, LOW);
+  digitalWrite(Left_Yellow, HIGH);delay(100);digitalWrite(Left_Yellow, LOW);
+  digitalWrite(Left_Green, HIGH);delay(100);digitalWrite(Left_Green, LOW);
+  digitalWrite(Down_Red, HIGH);delay(100);digitalWrite(Down_Red, LOW); 
+  digitalWrite(Down_Yellow, HIGH);delay(100);digitalWrite(Down_Yellow, LOW);
+  digitalWrite(Down_Green, HIGH);delay(100);digitalWrite(Down_Green, LOW);
+/*
+  digitalWrite(Right_Green, HIGH);  digitalWrite(Left_Green, HIGH);
+  digitalWrite(Up_Red, HIGH);  digitalWrite(Down_Red, HIGH);
+
+  digitalWrite(Up_Yellow, LOW);   digitalWrite(Down_Yellow, LOW);
+  digitalWrite(Left_Yellow, LOW);   digitalWrite(Right_Yellow, LOW);
+  digitalWrite(Left_Red, LOW);  digitalWrite(Right_Red, LOW);
+  digitalWrite(Up_Green, LOW);  digitalWrite(Down_Green, LOW);
   
-  digitalWrite(LEFT_YELLOW, HIGH);
-  digitalWrite(RIGHT_YELLOW, HIGH);
-  PrintWalkSign(delayTime);
-  digitalWrite(LEFT_YELLOW, LOW);
-  digitalWrite(RIGHT_YELLOW, LOW);
-  PrintWalkSign(delayTime);
-}
+  TimeUD = - DRIVABLE_DURATION;
+  TimeLR = millis();
+*/ }
 
-void PrintWalkSignWithPolling(int duration)
-{
-  unsigned timeStart;
-  unsigned timeEnd;
-  timeStart = timeEnd = millis();
-  while (timeEnd - timeStart <= duration)
+void checkDotmatrix(){
+for(int thisRow=7; thisRow>=0; thisRow--)
   {
-    DisplayWalkSign();
-    //Polling
-    timeEnd = millis();
-  }
-}
-
-void AdjustTrafficLightForLR()
-{
-  for (int t = (TRAFFIC_LIGHT_IDLE_TIME + LED_YELLOW_BLINK_DURATION) / 1000; t > 0; --t)
-  {
-    if (t > BLINK_TIME)
+    for(int thisCol=7; thisCol>=0; thisCol--)
     {
-      //1초간 보행 신호 출력
-      PrintWalkSign(1000);
-    }
+      digitalWrite(timePinRow[thisRow],HIGH);
+      digitalWrite(signPinRow[thisRow],HIGH);
+      digitalWrite(timePinCol[thisCol],LOW);
+      digitalWrite(signPinCol[thisCol],LOW);
+      delay(10);
+      digitalWrite(timePinRow[thisRow],LOW);
+      digitalWrite(signPinRow[thisRow],LOW);
+      digitalWrite(timePinCol[thisCol],HIGH);
+      digitalWrite(signPinCol[thisCol],HIGH);
+      delay(10);    
+      }
+  }
+  }
+
+
+
+void LR_BlinkYellow() //양옆 노란불 점멸과 동시에 보행가능신호
+{
+  static const int delayTime = LED_Yellow_BLINK_DURATION / (BLINK_TIME + BLINK_TIME); // delayTime 은 500u
+  //점멸시간동안 보행자신호 출력 + 점멸
+  digitalWrite(Left_Yellow, HIGH);  
+  digitalWrite(Right_Yellow, HIGH);
+  DisplayWalkSign(delayTime);
+  digitalWrite(Left_Yellow, LOW);
+  digitalWrite(Right_Yellow, LOW);
+  DisplayWalkSign(delayTime);
+}
+
+/*void LR_LED()//3초의 대기시간동안보행가능신호 5초의 시간동안 LR_BlinkYellow() 그후에 상태를 정지신호와 true설정//사용되지않고있음!!!
+{
+  for (int t = (LED_DELAY_TIME + LED_Yellow_BLINK_DURATION) / 1000; t > 0; --t) (대기시간3초+점멸시간5초)총 8초간진행
+  {
+    if (t > BLINK_TIME) //처음엔 8>5, 7>5, 6>5    이 3번(3초)의 시간동안 대기(3)
+      //1초간 보행 신호 출력 현재 총 3초 
+      DisplayWalkSign(1000); //  1초씩 보행가능신호 출력
     else
     {
-      digitalWrite(LEFT_GREEN, LOW);
-      digitalWrite(RIGHT_GREEN, LOW);
-      BlinkLEDForLR();
+      digitalWrite(Left_Green, LOW);
+      digitalWrite(Right_Green, LOW);
+      LR_BlinkYellow();//보행자 신호를 출력과 동시에 노란불 점멸해주는 함수 5초간진행(5)
     }
   }
-  digitalWrite(LEFT_RED, HIGH);
-  digitalWrite(RIGHT_RED, HIGH);
-  PrintWalkSign(WALK_DURATION);
-  gbChange = true;
+  digitalWrite(Left_Red, HIGH);
+  digitalWrite(Right_Red, HIGH);
+  DisplayStopSign(WALK_DURATION);
+  state = true;
+}*/
+
+void UD_BlinkYellow(int former, int later) //위아래 노란불 점멸과 동시에 정지신호+시간출력
+{
+  static const int delayTime = LED_Yellow_BLINK_DURATION / (BLINK_TIME + BLINK_TIME); //delayTime = 500u
+                                     //위아래 점멸과 동시에 정지신호와 시간 출력
+  digitalWrite(Up_Yellow, HIGH);
+  digitalWrite(Down_Yellow, HIGH);
+  DisplayStopSign(delayTime, former, later);
+  digitalWrite(Up_Yellow, LOW);
+  digitalWrite(Down_Yellow, LOW);
+  DisplayStopSign(delayTime, former, later);
 }
 
-void BlinkLEDForUD(int former, int later)
+void UD_LED() //3초의 대기시간동안 정지신호+숫자, 5초의 시간동안 UD_BlinkYellow() 그후에 상태를 (위아래정지 양옆파란불)★false설정해야하는것을 true설정을 해놓았다.
 {
-  static const int delayTime = LED_YELLOW_BLINK_DURATION / (BLINK_TIME + BLINK_TIME);
-  
-  digitalWrite(UPPER_YELLOW, HIGH);
-  digitalWrite(LOWER_YELLOW, HIGH);
-  PrintStopSign(delayTime, former, later);
-  digitalWrite(UPPER_YELLOW, LOW);
-  digitalWrite(LOWER_YELLOW, LOW);
-  PrintStopSign(delayTime, former, later);
-}
-
-void AdjustTrafficLightForUD()
-{
-  for (int t = (TRAFFIC_LIGHT_IDLE_TIME + LED_YELLOW_BLINK_DURATION) / 1000; t > 0; --t)
+  for (int t = (LED_DELAY_TIME + LED_Yellow_BLINK_DURATION) / 1000; t > 0; --t)// t는 현재 8인데 if문3번돌고 else문 5번돈뒤 상태를(위아래 정지, 양옆 주행) 
   {
     if (t > BLINK_TIME)
-    {
       //1초간 타이머 출력
-      PrintStopSign(1000, t / 10, t % 10);
-    }
+      DisplayStopSign(1000, t / 10, t % 10);//1번씩 십의자리숫자와 일의자리숫자에맞게 숫자와 정지표시를 출력
     else
     {
-      digitalWrite(UPPER_GREEN, LOW);
-      digitalWrite(LOWER_GREEN, LOW);
-      BlinkLEDForUD(t / 10, t % 10);
+      digitalWrite(Up_Green, LOW);
+      digitalWrite(Down_Green, LOW);
+      UD_BlinkYellow(t / 10, t % 10);
     }
   }
-  digitalWrite(UPPER_RED, HIGH);
-  digitalWrite(LOWER_RED, HIGH);
-  digitalWrite(LEFT_GREEN, HIGH);
-  digitalWrite(RIGHT_GREEN, HIGH);
-  digitalWrite(RIGHT_RED, LOW);
-  digitalWrite(LEFT_RED, LOW);
-  PrintWalkSign(WALK_DURATION);
-  gbChange = true;
+  digitalWrite(Up_Red, HIGH);//상태를 위아래 정지 양옆 파란불 = false가됨
+  digitalWrite(Down_Red, HIGH);
+  digitalWrite(Left_Green, HIGH);
+  digitalWrite(Right_Green, HIGH);
+  digitalWrite(Right_Red, LOW);
+  digitalWrite(Left_Red, LOW);
+  DisplayWalkSign(WALK_DURATION);
+  state = true;
 }
 
-void PrintStopSignWithPolling(int duration, int former, int later)
+void DisplayStopSignWithSwitch(int duration, int former, int later)//스위치 작동과 현재 위아래 주행가능하면(true일때) UD_LED실행, 정지신호와 숫자표시하는 함수
 {
   unsigned timeStart;
   unsigned timeEnd;
   timeStart = timeEnd = millis();
   while (timeEnd - timeStart <= duration)
   {
-    DisplayStopSign();
-    DisplayNumber(gNumbers[former], gNumbers[later]);
-    //Polling
-    if (digitalRead(SW) == LOW && digitalRead(UPPER_GREEN) == HIGH && digitalRead(LOWER_GREEN) == HIGH)
-    {
-      AdjustTrafficLightForUD();
-    }
+    StopSign();
+    DisplayNumber(Numbers[former], Numbers[later]);
+    //Switch ON
+    if (digitalRead(SW) == LOW && digitalRead(Up_Green) == HIGH && digitalRead(Down_Green) == HIGH)
+      UD_LED();//8초간 3초대기시간 +5초의 위아래점멸 후 
     timeEnd = millis();
   }
 }
 
-void ClearUpper()
+void ClearUpperDisplay()//위 DOT_MATRIX CLEAR
 {
   for (int i = 0; i < ARRAY_LENGTH; ++i)
   {
@@ -413,7 +409,7 @@ void ClearUpper()
   }
 }
 
-void ClearLower()
+void ClearLowerDisplay()//아래 DOT_MATRIX CLEAR
 {
   for (int i = 0; i < ARRAY_LENGTH; ++i)
   {
@@ -422,11 +418,11 @@ void ClearLower()
   }
 }
 
-void DisplayNumber(int former[][4], int later[][4])
+void DisplayNumber(int former[][4], int later[][4])//숫자를 표시하는 함수
 {
   for(int r = 0; r < 8; ++r)
   {
-    ClearLower();
+    ClearLowerDisplay();
     digitalWrite(signPinRow[r], HIGH);
     //십의 자리 출력
     for(int c = 0; c < 4; ++c)
@@ -441,42 +437,42 @@ void DisplayNumber(int former[][4], int later[][4])
     {
       if (later[r][c])
       {
-        digitalWrite(signPinCol[c + 4], LOW);
+        digitalWrite(signPinCol[c + 4], LOW);// [C+4]는 앞의 행4개를 뛰어넘고 일의자리위치에 표기하기위해
       }
     }
-    //HACK : 이 함수가 없을 시, 출력 신호가 뚜렷하게 보이지 않음
-    delay(1);
+    
+    delay(1);// 이 함수가 없을 시, 출력 신호가 뚜렷하게 보이지 않음 FOR문을통해 전환시 사이사이에 흐릿해질수 있음
   }
 }
 
-void DisplayStopSign()
+void StopSign()// 정지표시를 핀입력으로 주는 함수
 {
   for(int r = 0; r < 8; ++r)
   {
-    ClearUpper();
+    ClearUpperDisplay();
     digitalWrite(timePinRow[r], HIGH);
     for(int c = 0; c < 8; ++c)
     {
-      if(gWalkerSigns[0][r][c])
+      if(WalkerSigns[0][r][c])
       {
         digitalWrite(timePinCol[c], LOW);
       }
     }
-    //HACK : 출력 신호를 유지하기 위해 사용
-    delay(1);
+   
+    delay(1);// 이 함수가 없을 시, 출력 신호가 뚜렷하게 보이지 않음 FOR문을통해 전환시 사이사이에 흐릿해질수 있음
   }
 }
 
-void DisplayWalkSign()
+void WalkSign() //보행가능표시를 핀입력주는 함수
 {
   for(int r = 0; r < 8; ++r)
   {
-    ClearUpper();
-    ClearLower();
+    ClearUpperDisplay();
+    ClearLowerDisplay();
     digitalWrite(signPinRow[r], HIGH);
     for(int c = 0; c < 8; ++c)
     {
-      if(gWalkerSigns[1][r][c])
+      if(WalkerSigns[1][r][c])
       {
         digitalWrite(signPinCol[c], LOW);
       }
@@ -486,29 +482,28 @@ void DisplayWalkSign()
   }
 }
 
-void PrintStopSign(int duration, int former, int later)
+void DisplayStopSign(int duration, int former, int later)//(정지신호+숫자표기)+시간조건
 {
   unsigned timeStart;
   unsigned timeEnd;
   timeStart = timeEnd = millis();
   while (timeEnd - timeStart <= duration)
   {
-    DisplayStopSign();
-    DisplayNumber(gNumbers[former], gNumbers[later]);
+    StopSign();
+    DisplayNumber(Numbers[former], Numbers[later]);
     timeEnd = millis();
   }
 }
 
-void PrintWalkSign(unsigned duration)
+void DisplayWalkSign(unsigned duration)//(보행신호)+시간조건
 {
-  ClearUpper();
+  ClearUpperDisplay();
   unsigned timeStart;
   unsigned timeEnd;
   timeStart = timeEnd = millis();
   while (timeEnd - timeStart <= duration)
   {
-    DisplayWalkSign();
+    WalkSign();
     timeEnd = millis();
   }
 }
-
